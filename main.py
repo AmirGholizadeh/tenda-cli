3beb00abcbea22f58e8ef137955acdddd600944a
AmirGholizadeh <amir110gholizadeh@gmail.com>
2021-09-19 10:40:56 +0430

    fix: minor typo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def getArguments():
     "adding arguments to get from the user"
     parser = argparse.ArgumentParser()
     parser.add_argument("--username", "-u", dest="username", help="the username to the gateway", required=True)
-    parser.add_argument("--password" "-p", dest="password", help="the password to the gateway", required=True)
+    parser.add_argument("--password", "-p", dest="password", help="the password to the gateway", required=True)
     parser.add_argument("--url", dest="url", help="the url to the gateway", required=True)
     return parser.parse_args()
 
